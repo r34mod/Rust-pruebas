d5b8a545d7e1cf1f829517f518c86ea9c4526cd8
ierai <zondacamarafx10@gmail.com>
2024-04-25 22:29:41 +0200

    new fn

--- primera-prueba/src/primer-principal.rs
+++ primera-prueba/src/primer-principal.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+use rand::distributions::{Alphanumeric, DistString};
 
 fn main() {
     let palabra = "Esto es un texto";
@@ -11,6 +13,7 @@ fn main() {
         my_float = my_float.atan2(1.1);
         println!("{my_float}");
         arrays();
+        mapas();
 
     }else {
         print!("No check")
@@ -21,6 +24,27 @@ fn main() {
 fn arrays() {
     let mut my_array: Vec<&str> = Vec::new();
     my_array.push("Est");
-    print!("{:?}", my_array);
+    println!("{:?}", my_array);
+    let mut my_array2: HashSet<&str> = vec!["A","B","C","D"].into_iter().collect();
+    my_array2.insert("element");
+    println!("{:?}", my_array2);
+
+
+}
+
+
+fn mapas() {
+
+    let mut my_map: HashMap<&str, i32> = vec![("Pos1", 23)].into_iter().collect();
+    for x in 1..=10 {
+        let palabra = random_words();
+        my_map.insert(palabra, x);
+        println!("{:?}", my_map);
+    }
+    
+}
+
+fn random_words() -> &str {
+    let _random_letters = Alphanumeric.sample_string(&mut rand::thread_rng(),16);
 
 }
